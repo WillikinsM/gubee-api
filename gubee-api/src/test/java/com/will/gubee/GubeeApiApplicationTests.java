b66d72e279c4c0a3c114cac557805f6428fd639f
WillikinsM <willikins98@gmail.com>
2021-07-21 20:10:46 -0300

    commit inicial

---
+++ gubee-api/src/test/java/com/will/gubee/GubeeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.will.gubee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GubeeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
